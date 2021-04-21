b0b61db70c08ba679edef2c8daca86263e394104
Olamilekan Rasaq Aremu <aremulekan@gmail.com>
2021-04-22 02:53:46 +0300

    Home page finished

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {title : "Internet Clipboard"});
 });
+
 
 module.exports = router;
